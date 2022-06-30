222c8e478a40181167923d57b3a8549c42a9c761
Tom Yuval <tomuvak@gmail.com>
2022-06-30 12:51:48 +0200

    Gradle script (tool): add support for local properties and for publishing (to GitHub Packages)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
+import java.util.Properties
+
 plugins {
     kotlin("multiplatform") version "1.7.10"
+    `maven-publish`
 }
 
 group = "com.tomuvak.template"
 version = "0.0.1-SNAPSHOT"
+
+val localProperties = Properties()
+project.rootProject.file("local.properties").takeIf { it.canRead() }?.inputStream()?.let(localProperties::load)
+fun local(key: String): String? = localProperties.getProperty(key)
 
 repositories {
     mavenCentral()
@@ -52,5 +59,19 @@ kotlin {
         val macosX64Test by getting { dependsOn(nativeTest) }
         val mingwX64Main by getting { dependsOn(nativeMain) }
         val mingwX64Test by getting { dependsOn(nativeTest) }
+    }
+}
+
+local("githubRepository")?.let { githubRepository ->
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://maven.pkg.github.com/$githubRepository")
+                credentials {
+                    username = local("githubUser")
+                    password = local("githubToken")
+                }
+            }
+        }
     }
 }
